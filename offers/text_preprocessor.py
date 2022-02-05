5d443fa19f45ec9f33669210977a0bea1d205c21
Dominik Mackiewicz <dominik8881@wp.pl>
2022-02-05 15:36:41 +0100

    text_preprocessor update

--- offers/text_preprocessor.py
+++ offers/text_preprocessor.py
@@ -1,18 +1,14 @@
 # %%
 import spacy
-from offers.singleton import SingletonMeta as __SingletonMeta
+from singleton import SingletonMeta as __SingletonMeta
 
 
 class TextPreprocessor(metaclass=__SingletonMeta):
     __nlp: spacy
 
     def __init__(self):
-        self.__nlp = spacy.load("pl_core_news_lg")
+        self.__nlp = spacy.load("pl_core_news_md")
         self.all_stopwords = self.__nlp.Defaults.stop_words
 
     def lemmatize(self, sentence: str) -> str:
-        lematized_sentence: str = ''
-        for w in self.__nlp(sentence):
-            if not w.text.lower() in self.all_stopwords:
-                lematized_sentence += w.lemma_ + ' '
-        return lematized_sentence
+        return ' '.join([w.lemma_ for w in self.__nlp(sentence) if w not in self.all_stopwords])
