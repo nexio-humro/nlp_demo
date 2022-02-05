68a2a3498d327f9727bdea5789148f97bbbf59d6
Dominik Mackiewicz <dominik8881@wp.pl>
2022-02-05 15:46:56 +0100

    new api

--- offers/chatbot_api.py
+++ offers/chatbot_api.py
@@ -7,52 +7,89 @@ import nltk
 import numpy as np
 from tensorflow import keras
 
-from offers.text_preprocessor import TextPreprocessor
+from text_preprocessor import TextPreprocessor
 
 
 class Chatbot:
-
-    # nltk.download('punkt')
     __text_preprocessor: TextPreprocessor
     __tag_history = []
     __data = None
     __model = None
+    
+    def __init__(self, threshold=0.55):
+        self.threshold = threshold
 
-    def __init__(self):
+        words = []
         print("chatbot-offers init")
         self.__text_preprocessor = TextPreprocessor()
         # %%
         current_dir = os.path.dirname(os.path.abspath(__file__))
-        with open(current_dir + '/data/data.json', encoding='utf8') as file:
+        with open(current_dir + '\data\data.json', encoding='utf8') as file:
             self.__data = json.load(file)
 
-        with open(current_dir + '/data/data.pickle', 'rb') as f:
+        with open(current_dir + '\data\data.pickle', 'rb') as f:
             self.__words, self.__labels, self.__training, self.__output = pickle.load(f)
 
-        self.__model = keras.models.load_model(current_dir + '/models/model')
+        self.__model = keras.models.load_model(current_dir + '/models/model/chatbot.keras')
+        for intent in self.__data["intents"]:
+            for pattern in intent["patterns"]:
+                    tokenized_pattern = nltk.word_tokenize(pattern.lower())
+                    words.extend(tokenized_pattern)
+
+        words = [self.__text_preprocessor.lemmatize(w) for w in words]
+        words = sorted(list(set(words)))
 
     # %%
 
-    def bag_of_words(self, sentence: str):
-        lematized_sentence: str = self.__text_preprocessor.lemmatize(sentence)
-        s_words = nltk.word_tokenize(lematized_sentence)
-        bag = np.array([1 if word in s_words else 0 for word in self.__words])
-        return bag
+    def bag_of_words(self, sentence, words):
+        bag = [0 for _ in range(len(words))]
+        sentence_words = nltk.word_tokenize(sentence)
+        sentence_words = [self.__text_preprocessor.lemmatize(word.lower()) for word in sentence_words]
+
+        for s in sentence_words:
+            for i, w in enumerate(words):
+                if w == s:
+                    bag[i] = 1
+        return np.array(bag)
 
     # %%
-    def chat(self, cause_text: str):
-            results = self.__model.predict(self.bag_of_words(cause_text))
+    def question(self, cause_text: str):
+        bag =  self.bag_of_words(sentence=cause_text, words=self.__words)
+        results = self.__model.predict(np.array([bag]))
+        results_index = np.argmax(results)
+        tag = self.__labels[results_index]
+                    
+        if results[0][results_index] > self.threshold:
+            for t in self.__data["intents"]:
+                if t['tag'] == tag:
+                    responses = t['responses']
+                    print(random.choice(responses))
+        else:
+            print("Nie zrozumiałem, proszę zadaj inne pytanie.")
+
+    def chat(self):
+        print("Nexiobot na Ciebie czeka! [napisz 'koniec' aby zakończyć rozmowę]")
+        while True:
+            user_input = input("You:")
+            if user_input.lower() == "koniec":
+                break
+            
+            bag =  self.bag_of_words(sentence=user_input, words=self.__words)
+
+            results = self.__model.predict(np.array([bag]))
             results_index = np.argmax(results)
             tag = self.__labels[results_index]
+                        
+            if results[0][results_index] > self.threshold:
+                for t in self.__data["intents"]:
+                    if t['tag'] == tag:
+                        responses = t['responses']
+                        print(random.choice(responses))
+            else:
+                print("Nie zrozumiałem, proszę zadaj inne pytanie.")
 
-            responses = []
-            for tg in self.__data["intents"]:
-                if tg['tag'] == tag:
-                    responses = tg['responses']
-                    break
 
-            print(tag)
-            print(responses[0:])
-            return random.choice(responses)
 chatbot: Chatbot = Chatbot()
-chatbot.chat(cause_text="No siema mordo")
+
+# chatbot.question("siema przedstaw się")
+chatbot.chat()
